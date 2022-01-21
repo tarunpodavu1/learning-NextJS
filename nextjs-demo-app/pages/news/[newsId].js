469ba624cd3dadd9f50bea641a726cf1db28abef
trX-X <tarinkumarpodavu4@gmail.com>
2022-01-21 11:53:11 +0530

    learned Routing and Nested Routing

---
+++ nextjs-demo-app/pages/news/[newsId].js
@@ -0,0 +1,12 @@
+import { useRouter } from "next/router";
+
+const DetailPage = () => {
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+  console.log(newsId);
+
+  return <h1>The Detail Page</h1>;
+};
+
+export default DetailPage;
--- nextjs-demo-app/pages/news/index.js
+++ nextjs-demo-app/pages/news/index.js
@@ -1,5 +1,17 @@
+import Link from "next/link";
+
 const NewsPage = () => {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <Link href="/news/next-js-is-a-great-framework">
+          <li>NextJS is great Framework</li>
+        </Link>
+        <li>Great article</li>
+      </ul>
+    </>
+  );
 };
 
 export default NewsPage;
